8f48ae34eeef4080ec6102a10729743f28f754d2
brenoml <breno.lagatta@hotmail.com>
2020-06-05 01:55:22 -0300

    heroku

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirnameame + '/dist/appgit'));
+app.use(express.static(__dirname + '/dist/appgit'));
 
 app.get('/*', (req,res) => {
-    res.sendFile(__dirnameame + '/dist/appgit/index.html');
+    res.sendFile(__dirname + '/dist/appgit/index.html');
 });
 
 app.listen(PORT, () => {
